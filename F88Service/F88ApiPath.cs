e4b5e4cce6189583c346acf3d6d78034cfe23857
quangphat <phat.dinhquang@haravan.com>
2019-12-03 19:13:41 +0700

    f

--- F88Service/F88ApiPath.cs
+++ F88Service/F88ApiPath.cs
@@ -8,7 +8,7 @@ namespace F88Service
 {
     public static class F88ApiPath
     {
-        //public static readonly string F88BasePath = "http://118.70.129.116:1809";
-        public static readonly string F88BasePath = "https://apilienket.f88.vn";
+        public static readonly string F88BasePath = "http://118.70.129.116:1809";
+        //public static readonly string F88BasePath = "https://apilienket.f88.vn";
     }
 }
