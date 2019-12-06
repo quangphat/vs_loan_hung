514e83fb739c5b51790503955ddfee9af66ea91a
quangphat <phat.dinhquang@haravan.com>
2019-12-06 22:37:26 +0700

    employee

---
+++ VS_LOAN.Core.Business/EmployeeBusiness.cs
@@ -0,0 +1,55 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VS_LOAN.Core.Entity.Employee;
+
+namespace VS_LOAN.Core.Business
+{
+    public class EmployeeBusiness :BaseBusiness
+    {
+        public async Task<int> Count(
+            DateTime workFromDate,
+            DateTime workToDate,
+            int roleId,
+            string freeText)
+        {
+            var p = new DynamicParameters();
+            p.Add("workFromDate", workFromDate);
+            p.Add("workToDate", workToDate);
+            p.Add("roleId", roleId);
+            p.Add("freeText", freeText);
+            using (var con = GetConnection())
+            {
+                var total = await con.ExecuteScalarAsync<int>("sp_CountNhanvien", p, commandType: CommandType.StoredProcedure);
+                return total;
+            }
+
+        }
+        public async Task<List<EmployeeViewModel>> Gets(
+            DateTime workFromDate,
+            DateTime workToDate,
+            int roleId,
+            string freeText,
+            int page,
+            int limit)
+        {
+            var p = new DynamicParameters();
+            p.Add("workFromDate", workFromDate);
+            p.Add("workToDate", workToDate);
+            p.Add("freeText", freeText);
+            p.Add("page", page);
+            p.Add("roleId", roleId);
+            p.Add("limit", limit);
+            using (var con = GetConnection())
+            {
+                var results = await con.QueryAsync<EmployeeViewModel>("sp_GetNhanvien", p, commandType: CommandType.StoredProcedure);
+                return results.ToList();
+            }
+
+        }
+    }
+}
---
+++ VS_LOAN.Core.Entity/Employee/EmployeeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_LOAN.Core.Entity.Employee
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Code { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public DateTime? WorkDate { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public DateTime? CreatedTime { get; set; }
+    }
+}
