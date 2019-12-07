cee4e77681126e2a50140a6b8109c5a3aaf92906
quangphat <phat.dinhquang@haravan.com>
2019-12-07 22:20:42 +0700

    f

--- VS_LOAN.Core.Web/Helpers/Menu.cs
+++ VS_LOAN.Core.Web/Helpers/Menu.cs
@@ -91,7 +91,7 @@ namespace VS_LOAN.Core.Web.Helpers
                     new SiteMenuItem("Quản lý hồ sơ","menu-icon qlhs",IndexMenu.M_2,"#",new int[] { }),
                         new SiteMenuItem("Danh sách hồ sơ","", IndexMenu.M_2_2,QuanLyHoSoController.LstRole["DanhSachHoSo"]._href, QuanLyHoSoController.LstRole["DanhSachHoSo"]._mangChucNang), // 1.3     
                         new SiteMenuItem("Duyệt hồ sơ","", IndexMenu.M_2_3,DuyetHoSoController.LstRole["Index"]._href, DuyetHoSoController.LstRole["Index"]._mangChucNang) ,// 1.2  
-                    new SiteMenuItem("Check Duplicate","menu-icon qlhs",IndexMenu.M_5,"#",new int[] { }),
+                    new SiteMenuItem("Check Duplicate","menu-icon duplicate",IndexMenu.M_5,"#",new int[] { }),
                         new SiteMenuItem("Tạo mới","", IndexMenu.M_5_1,CustomerController.LstRole["AddNew"]._href, CustomerController.LstRole["AddNew"]._mangChucNang), // 1.3     
                         new SiteMenuItem("Danh sách","", IndexMenu.M_5_2,CustomerController.LstRole["Index"]._href, CustomerController.LstRole["Index"]._mangChucNang) // 1.2  
                  
