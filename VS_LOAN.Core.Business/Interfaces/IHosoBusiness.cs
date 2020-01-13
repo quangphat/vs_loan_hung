7a493553abe66555590db91335002a9f02ec7eeb
phatdq <phatdq@sendo.vn>
2020-01-13 11:35:07 +0700

    interface hoso

---
+++ VS_LOAN.Core.Business/Interfaces/IHosoBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_LOAN.Core.Business.Interfaces
+{
+    public interface IHosoBusiness
+    {
+        Task<bool> UpdateF88Result(int hosoId, int f88Result, string reason);
+    }
+}
