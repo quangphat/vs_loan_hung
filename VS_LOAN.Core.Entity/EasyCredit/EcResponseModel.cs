cf2ff5efb6e2a2c70d7fdec38d2ccc2b33a13006
quangphat <phat.dinhquang@haravan.com>
2020-02-16 15:36:14 +0700

    EcResponseModel

---
+++ VS_LOAN.Core.Entity/EasyCredit/EcResponseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_LOAN.Core.Entity.EasyCredit
+{
+    public class EcResponseModel<T>
+    {
+        public string code { get; set; }
+        public string message { get; set; }
+        public object error { get; set; }
+        public int status { get; set; }
+        public T data { get; set; }
+    }
+    public class EcDataResponse
+    {
+        public string request_id { get; set; }
+        public string response_message { get; set; }
+        public string response_code { get; set; }
+    }
+}
