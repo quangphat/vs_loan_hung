c5a913ee7c79d22091eaa72c1fbcfe177e24146a
quangphat <phat.dinhquang@haravan.com>
2020-02-23 15:11:15 +0700

    CustomerModel

--- VS_LOAN.Core.Entity/CustomerModel.cs
+++ VS_LOAN.Core.Entity/CustomerModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace VS_LOAN.Core.Entity
 {
-        public class CustomerModel
+    public class CustomerModel : BaseSqlEntity
     {
         public int Id { get; set; }
         public string FullName { get; set; }
@@ -14,6 +14,18 @@ namespace VS_LOAN.Core.Entity
         public string Cmnd { get; set; }
         public int CICStatus { get; set; }
         public bool Gender { get; set; }
-        public string Note { get; set; }
+        public string MatchCondition { get; set; }
+        public string NotMatch { get; set; }
+        public string LastNote { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public DateTime? IssueDate { get; set; }
+        public string IssuePlaceCode { get; set; }
+        public int PermanentProvinceCode { get; set; }
+        public int PermanentDistrictCode { get; set; }
+        public int PermanentWardCode { get; set; }
+        public string PermanentAddress { get; set; }
+
     }
 }
