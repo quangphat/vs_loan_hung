9299263b3fc737ad7955d4b75cdc4461b0128655
quangphat <phat.dinhquang@haravan.com>
2020-07-06 22:07:41 +0700

    init, add field code to OptionSimple

--- VS_LOAN.Core.Entity/OptionSimple.cs
+++ VS_LOAN.Core.Entity/OptionSimple.cs
@@ -9,6 +9,7 @@ namespace VS_LOAN.Core.Entity
     public class OptionSimple
     {
         public int Id { get; set; }
+        public string Code { get; set; }
         public string Name { get; set; }
         public bool IsSelect { get; set; }
     }
