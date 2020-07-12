a8f3ed9b246a5099a18eb16f293936e82168ebb9
quangphat <phat.dinhquang@haravan.com>
2020-07-12 17:50:36 +0700

    test

---
+++ VS_LOAN.Core.Business/Interfaces/IMCreditRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VS_LOAN.Core.Entity.MCreditModels;
+
+namespace VS_LOAN.Core.Business.Interfaces
+{
+    public interface IMCreditRepositoryTest
+    {
+        Task<GetFileUploadResponse> GetFilesNeedToUpload(GetFileUploadRequest model);
+    }
+}
