9fa9920f1f9172d33821139e9562abe5d51984f6
quangphat <phat.dinhquang@haravan.com>
2020-07-16 02:03:31 +0700

    update to production

--- change_db.sql
+++ change_db.sql
@@ -1,13 +1,13 @@
 
-EXEC sp_rename 'TAI_LIEU_HS.Maloai', 'FileKey', 'COLUMN'
+EXEC sp_rename '[TAI_LIEU_HS].Ma_loai', 'FileKey', 'COLUMN'
 EXEC sp_rename 'TAI_LIEU_HS.DuongDan', 'FilePath', 'COLUMN'
 EXEC sp_rename 'TAI_LIEU_HS.Ten', 'FileName', 'COLUMN'
-EXEC sp_rename 'TAI_LIEU_HS.MaHS', 'ProfileId', 'COLUMN'
+EXEC sp_rename 'TAI_LIEU_HS.Ma_Ho_So', 'ProfileId', 'COLUMN'
 EXEC sp_rename 'TAI_LIEU_HS.typeId', 'ProfileTypeId', 'COLUMN'
-
+--done
 -------
 EXEC sp_rename 'LOAI_TAI_LIEU.HosoType', 'ProfileTypeId', 'COLUMN'
-
+--done
 -------------------------
 alter table Tai_lieu_HS
 add DocumentName nvarchar(500),
@@ -15,7 +15,7 @@ DocumentCode varchar(400),
 MC_DocumentId int,
 MC_MapBpmVar varchar(400),
 MC_GroupId int
-
+--done
 ----------------------
 ALTER procedure [dbo].[getTailieuByHosoId](@profileId int, @profiletTypeId int =1)
 
@@ -43,7 +43,7 @@ and tl.ProfileTypeId = @profiletTypeId
 order by ltl.Bat_Buoc desc
 
 end
-
+-------done
 ------------
 ALTER PROCEDURE [dbo].[sp_TAI_LIEU_HS_Them]
 	-- Add the parameters for the stored procedure here
@@ -59,6 +59,7 @@ BEGIN
 	insert TAI_LIEU_HS(FileKey,FileName,FilePath,ProfileId,ProfileTypeId)
 	values(@FileKey,@FileName,@FilePath,@ProfileId,@ProfileTypeId)
 END
+--done
 --------------
 ALTER PROCEDURE [dbo].[sp_TAI_LIEU_HS_XoaTatCa]
 	-- Add the parameters for the stored procedure here
@@ -70,6 +71,8 @@ BEGIN
 	-- interfering with SELECT statements.
 	delete from TAI_LIEU_HS where ProfileId=@ProfileId and ProfileTypeId = @ProfileTypeId
 END
+
+--done
 ---------------
 create table FilesGroup
 (
@@ -77,7 +80,7 @@ Id int identity(1,1) not null,
 Name nvarchar(200),
 IsRequire bit
 )
-
+--done
 ------------------
 
 ALTER procedure [dbo].[getTailieuByHosoId](@profileId int, @profiletTypeId int =1)
@@ -99,6 +102,8 @@ and tl.ProfileTypeId = @profiletTypeId
 order by ltl.Bat_Buoc desc
 
 end
+
+--done
 ----------------
 ALTER procedure [dbo].[updateExistingFile] 
 (@fileId int, @name nvarchar(200),@url nvarchar(max), @typeId int =1)
@@ -111,6 +116,8 @@ update TAI_LIEU_HS set FileName = @name,
 	where Id = @fileId and ProfileTypeId = @typeId
 
 end
+
+--done
 -----------------
 ALTER PROCEDURE [dbo].[sp_TAI_LIEU_HS_Them]
 	(@FileKey int,
@@ -133,7 +140,7 @@ BEGIN
 	values(@FileKey,@FileName,@FilePath,@ProfileId,@Deleted,@ProfileTypeId,@DocumentName,@DocumentCode,@MC_DocumentId,@MC_MapBpmVar,@MC_GroupId,@OrderId,@Folder)
 END
 
-
+--done
 -----------
 ALTER procedure [dbo].[updateExistingFile] 
 (@fileId int, @FileName nvarchar(200),@FilePath nvarchar(max), @ProfileTypeId int =1, @Folder nvarchar(max) = null)
@@ -147,6 +154,8 @@ update TAI_LIEU_HS set FileName = @FileName,
 	where Id = @fileId and ProfileTypeId = @ProfileTypeId
 
 end
+
+--done
 ----------
 ALTER procedure [dbo].[getTailieuByHosoId](@profileId int, @profileTypeId int =1)
 
@@ -175,7 +184,7 @@ and tl.ProfileTypeId = @profileTypeId
 order by ltl.Bat_Buoc desc
 
 end
-
+--done
 
 ------------
 ALTER procedure [dbo].[getTailieuByHosoId](@profileId int, @profileTypeId int =1)
@@ -211,6 +220,8 @@ begin
 		order by ltl.Bat_Buoc desc
 end;
 end
+
+--done
 ---------------
 create procedure [dbo].[getTailieuByMCId](@mcId varchar(20))
 
@@ -234,6 +245,8 @@ begin
 		tl.MC_GroupId
 	from TAI_LIEU_HS tl where MCId = @mcId and ProfileTypeId = 3
 end
+
+--done
 -------------------
 
 ALTER procedure [dbo].[sp_TaiLieuHoso_UpdateMCId] (@profileId int, @mcId varchar(20))
@@ -241,4 +254,6 @@ as
 begin
 update TAI_LIEU_HS set MCId = @mcId where ProfileId = @profileId and ProfileTypeId =3 
 end
+
+--done
 -------------
