bd10950858d4bf6efd2da90388aa94573738d4c1
quangphat <phat.dinhquang@haravan.com>
2020-07-19 15:20:41 +0700

    repo register DI

---
+++ VietStar.Repository/Infrastructures/DependencyRegister.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VietStar.Repository.Interfaces;
+
+namespace VietStar.Repository.Infrastructures
+{
+    public static class DependencyRegister
+    {
+        public static void RegisterRepository(this IServiceCollection services)
+        {
+            services.AddSingleton<IEmployeeRepository, EmployeeRepository>();
+        }
+    }
+}
