a2d1941d21fa97d5189ef4263b498dfaddf0530d
quangphat <phat.dinhquang@haravan.com>
2020-07-19 16:37:38 +0700

    Basecontroller

--- VietStar.Client/Controllers/AccountController.cs
+++ VietStar.Client/Controllers/AccountController.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using VietStar.Business.Interfaces;
+using VietStar.Entities.Infrastructures;
+using VietStar.Entities.ViewModels;
 
 namespace VietStar.Client.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController : VietStarBaseController
     {
+        protected readonly IEmployeeBusiness _bizEmployee;
+        public AccountController(IEmployeeBusiness employeeBusiness, CurrentProcess process) : base(process)
+        {
+            _bizEmployee = employeeBusiness;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +28,34 @@ namespace VietStar.Client.Controllers
         {
             return View();
         }
-        
+        public async Task<IActionResult> LoginApi(LoginModel model)
+        {
+            var account = await _bizEmployee.Login(model);
+            if (account == null)
+                return ToResponse(false);
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim("Id", account.Id.ToString()));
+            claims.Add(new Claim("UserName", account.UserName));
+            if (!string.IsNullOrWhiteSpace(account.FullName))
+                claims.Add(new Claim("FullName", account.FullName));
+            if (!string.IsNullOrWhiteSpace(account.Email))
+                claims.Add(new Claim("Email", account.Email));
+            if (!string.IsNullOrWhiteSpace(account.Code))
+                claims.Add(new Claim("Code", account.Code));
+            if (!string.IsNullOrWhiteSpace(account.Rolecode))
+                claims.Add(new Claim("RoleCode", account.Rolecode));
+
+            
+            var userIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = model.Rememberme
+            };
+            await HttpContext.SignInAsync(principal, authProperties);
+            string url = "/Home/Index";
+            return ToResponse(url);
+        }
     }
 }
---
+++ VietStar.Client/Controllers/VietStarBaseController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VietStar.Entities.Infrastructures;
+
+namespace VietStar.Client.Controllers
+{
+    public class VietStarBaseController : Controller
+    {
+        public readonly CurrentProcess _process;
+        public VietStarBaseController(CurrentProcess process)
+        {
+            _process = process;
+        }
+        private JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Include,
+            ContractResolver = new DefaultContractResolver()
+        };
+
+        public IActionResult ToResponse()
+        {
+            var model = new ResponseJsonModel();
+            bool hasError = _checkHasError(model);
+            return Json(model, _jsonSerializerSettings);
+        }
+        public IActionResult ToResponse<T>(T data)
+        {
+            var model = new ResponseJsonModel<T>();
+            if (!_checkHasError(model))
+                model.data = data;
+
+            return Json(model, _jsonSerializerSettings);
+        }
+        public IActionResult ToResponse(bool isSuccess)
+        {
+            var model = new ResponseJsonModel();
+
+            if (!_checkHasError(model))
+                model.success = isSuccess;
+
+            return Json(model, _jsonSerializerSettings);
+        }
+        private bool _checkHasError(ResponseJsonModel model)
+        {
+            var hasError = _process.HasError;
+            if (hasError)
+            {
+                var errorMessage = _process.ToError();
+
+                model.error = new ErrorJsonModel()
+                {
+                    Result = false,
+                    code = errorMessage.Message,
+                    trace_keys = errorMessage.TraceKeys
+                };
+            }
+            model.success = !hasError;
+            return hasError;
+        }
+    }
+    public class ResponseJsonModel
+    {
+        public ErrorJsonModel error { get; set; }
+        public bool success { get; set; }
+    }
+
+    public class ResponseJsonModel<T> : ResponseJsonModel
+    {
+        public T data { get; set; }
+    }
+    public class ErrorJsonModel
+    {
+        public string code { get; set; }
+        public List<object> trace_keys { get; set; }
+        public bool Result { get; set; }
+    }
+}
