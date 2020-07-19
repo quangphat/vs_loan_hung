43e212138643ff82af14ae23c020adb2c04444e5
quangphat <phat.dinhquang@haravan.com>
2020-07-19 15:03:47 +0700

    add VietStar.Utility

---
+++ VietStar.Utility/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VietStar.Utility
+{
+    public static class Utils
+    {
+        static string start = "KPMG_EV";
+        static string end = "KPMG_PM";
+        public static string getMD5(string data)
+        {
+            return BitConverter.ToString(encryptData(start + data + end)).Replace("-", "").ToLower();
+        }
+        private static byte[] encryptData(string data)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5Hasher = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] hashedBytes;
+            System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
+            hashedBytes = md5Hasher.ComputeHash(encoder.GetBytes(data));
+            return hashedBytes;
+        }
+    }
+}
