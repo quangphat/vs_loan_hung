8fbbe9ca005396965cebc37f307c009450960276
quangphat <phat.dinhquang@haravan.com>
2020-07-19 14:38:17 +0700

    init VietStar.Business

---
+++ VietStar.Business/BaseBusiness.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VietStar.Entities.Infrastructures;
+
+namespace VietStar.Business
+{
+    public abstract class BaseBusiness
+    {
+        public readonly CurrentProcess _process;
+        protected IMapper _mapper;
+        public BaseBusiness(IMapper mapper, CurrentProcess process)
+        {
+            _process = process;
+            _mapper = mapper;
+        }
+        public void AddError(string errorMessage, params object[] traceKeys)
+        {
+            _process.AddError(errorMessage, traceKeys);
+        }
+        public bool CheckIsNotLogin()
+        {
+            if (string.IsNullOrWhiteSpace(_process.User.UserName))
+            {
+                //AddError(errors.error_login_expected);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ VietStar.Business/Interfaces/IEmployeeBusiness.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VietStar.Business.Interfaces
+{
+    public class IEmployeeBusiness
+    {
+    }
+}
