ce0f3eae8da0b505d1af25245ae101583339a135
quangphat <phat.dinhquang@haravan.com>
2020-07-19 17:50:01 +0700

    add ProfileController, Index.cshtml

---
+++ VietStar.Client/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KingOffice.Infrastructures;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VietStar.Entities.Infrastructures;
+
+namespace VietStar.Client.Controllers
+{
+    public class ProfileController : VietStarBaseController
+    {
+        public ProfileController(CurrentProcess process) : base(process)
+        {
+        }
+        [MyAuthorize(Permissions ="dd")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
