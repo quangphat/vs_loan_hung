a42f15ee7c21d624dcb35a98037c1b73430f68b7
quangphat <phat.dinhquang@haravan.com>
2020-07-19 14:59:00 +0700

    bz login

---
+++ VietStar.Business/EmployeeBusiness.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using VietStar.Business.Interfaces;
+using VietStar.Entities.Infrastructures;
+using VietStar.Entities.Messages;
+using VietStar.Entities.ViewModels;
+using VietStar.Repository.Interfaces;
+
+namespace VietStar.Business
+{
+    public class EmployeeBusiness : BaseBusiness, IEmployeeBusiness
+    {
+        protected readonly IEmployeeRepository _rpEmployee;
+        public EmployeeBusiness(IEmployeeRepository employeeRepository,IMapper mapper, CurrentProcess process) : base(mapper, process)
+        {
+            _rpEmployee = employeeRepository;
+        }
+
+        public async Task<Account> Login(LoginModel model)
+        {
+            if(model==null)
+            {
+                AddError(Errors.invalid_data);
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(model.UserName) || string.IsNullOrWhiteSpace(model.Password))
+            {
+                //AddError($"connStr:{_rpEmployee.ConnectStr}, username: {username}, password: {password}");
+                AddError(Errors.username_or_password_must_not_be_empty);
+                return null;
+            }
+
+            var account = await _rpEmployee.Login(model.UserName, model.Password);
+
+            if (account == null)
+            {
+                AddError(Errors.invalid_username_or_pass);
+                return null;
+            }
+            
+            return account;
+        }
+    }
+}
--- VietStar.Business/Interfaces/IEmployeeBusiness.cs
+++ VietStar.Business/Interfaces/IEmployeeBusiness.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using VietStar.Entities.ViewModels;
 
 namespace VietStar.Business.Interfaces
 {
-    public class IEmployeeBusiness
+    public interface IEmployeeBusiness
     {
+        Task<Account> Login(LoginModel model);
     }
 }
