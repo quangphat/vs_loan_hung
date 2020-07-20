61895f3ace0697487ca2ae35d1d905feb311126e
phatdq <phatdq@sendo.vn>
2020-07-20 17:38:23 +0700

    enums profilestatus

---
+++ VietStar.Client/Models/Enums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VietStar.Client.Models
+{
+    public class Enums
+    {
+        public enum ProfileStatus
+        {
+            Draft =0,
+            New = 1,
+            Input = 2,
+            Expertise = 3,
+            Deny = 4,
+            Additional = 5,
+            Release =6,
+            Compared =7,
+            Cancel = 8,
+            PCB = 9,
+            Processing  =10,
+            Accept = 11,
+            Finish = 12,
+        }
+    }
+}
