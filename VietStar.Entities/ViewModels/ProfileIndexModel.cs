d1957e09319d9840b439d34c5c55136df6c83675
phatdq <phatdq@sendo.vn>
2020-07-20 16:10:58 +0700

    ProfileIndexModel

---
+++ VietStar.Entities/ViewModels/ProfileIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VietStar.Entities.Commons;
+
+namespace VietStar.Entities.ViewModels
+{
+    public class ProfileIndexModel : Pagination
+    {
+
+    }
+}
