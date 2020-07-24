fd12f7cef93afc480189952e0d25abb461ea2274
phatdq <phatdq@sendo.vn>
2020-07-24 16:04:02 +0700

    add RevokeController

---
+++ VS_LOAN.Core.Web/Controllers/RevokeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VS_LOAN.Core.Web.Controllers
+{
+    public class RevokeController : BaseController
+    {
+
+        public RevokeController():base()
+        {
+
+        }
+        // GET: Revoke
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
