38d7eb6003fd5c0a6f56f7699f12a6da60211094
phatdq <phatdq@sendo.vn>
2020-07-27 14:04:35 +0700

    Systemconfig

---
+++ VS_LOAN.Core.Entity/Constanst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_LOAN.Core.Entity
+{
+    public class Constanst
+    {
+        public const string revoke_debt_max_row_import = "revoke_debt_max_row_import";
+    }
+}
