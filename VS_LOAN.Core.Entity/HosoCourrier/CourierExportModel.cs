f51fa7913462872e5a4f7dc0f3d4fbcbfdba9a8d
quangphat <phat.dinhquang@haravan.com>
2020-08-03 12:44:53 +0700

    f

---
+++ VS_LOAN.Core.Entity/HosoCourrier/CourierExportModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_LOAN.Core.Entity.HosoCourrier
+{
+    public class CourierExportModel : Pagination
+    {
+        [Description("Họ tên")]
+        public string CustomerName { get; set; }
+        [Description("Số điện thoại")]
+        public string Phone { get; set; }
+        [Description("Cmnd")]
+        public string Cmnd { get; set; }
+        [Description("Trạng thái")]
+        public string Status { get; set; }
+        [Description("Ghi chú")]
+        public string LastNote { get; set; }
+        [Description("SaleCode")]
+        public string SaleCode { get; set; }
+        [Description("Nhân viên")]
+        public string AssignUser { get; set; }
+        [Description("Sản phẩm")]
+        public string ProductName { get; set; }
+        [Description("Người tạo")]
+        public string CreatedUser { get; set; }
+        [Description("Tỉnh")]
+        public string ProvinceName { get; set; }
+        [Description("Huyện")]
+        public string DistrictName { get; set; }
+        [Description("Ngày tạo")]
+        public string CreatedTime { get; set; }
+       
+    }
+}
---
+++ VS_LOAN.Core.Utility/StatusUtil.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VS_LOAN.Core.Entity;
+
+namespace VS_LOAN.Core.Utility
+{
+    public class StatusUtil
+    {
+        public static string ReturnStatusString(int status)
+        {
+            string result = "";
+            switch (status)
+            {
+                case (int)HosoCourierStatus.New:
+                    result = "Mới";
+                    break;
+                case (int)HosoCourierStatus.InProgress:
+                    result = "Đang xử lý";
+                    break;
+                case (int)HosoCourierStatus.Deny:
+                    result =  "Từ chối";
+                    break;
+                case (int)HosoCourierStatus.Accept:
+                    result = "Chấp nhận";
+                    break;
+                case (int)HosoCourierStatus.Giaingan:
+                    result = "Giải ngân";
+                    break;
+                case (int)HosoCourierStatus.Finish:
+                    result = "Hoàn thành";
+                    break;
+                case (int)HosoCourierStatus.Cancel:
+                    result = "Hủy";
+                    break;
+                case (int)HosoCourierStatus.Nhaplieu:
+                    result = "Nhập liệu";
+                    break;
+                case (int)HosoCourierStatus.Thamdinh:
+                    result = "Thẩm định";
+                    break;
+                case (int)HosoCourierStatus.BosungHoso:
+                    result = "Bổ sung hồ sơ";
+                    break;
+                case (int)HosoCourierStatus.DaDoichieu:
+                    result = "Đã đối chiếu";
+                    break;
+                case (int)HosoCourierStatus.PCB:
+                    result = "PCB";
+                    break;
+                default: result= "";
+                    break;
+            }
+            return result;
+        }
+    }
+}
