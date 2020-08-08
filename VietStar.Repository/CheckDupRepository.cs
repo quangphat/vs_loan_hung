11b1ba36fc1c6a2cc65df5555e915392265adbde
quangphat <phat.dinhquang@haravan.com>
2020-08-09 01:39:41 +0700

    CreateAsync checkdup

--- VietStar.Repository/CheckDupRepository.cs
+++ VietStar.Repository/CheckDupRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using VietStar.Entities;
+using VietStar.Entities.CheckDup;
 using VietStar.Entities.ViewModels;
 using VietStar.Repository.Interfaces;
 
@@ -20,9 +21,29 @@ namespace VietStar.Repository
         {
             _rpLog = logRepository;
         }
-        public async Task<RepoResponse<int>> CreateAsync()
+        public async Task<RepoResponse<int>> CreateAsync(CheckDupAddSql model, int createdBy)
         {
-            
+            var par = GetParams(model, new string[] {
+                nameof(model.UpdatedBy),
+                nameof(model.UpdatedTime),
+                nameof(model.CreatedTime),
+                nameof(model.CreatedBy)
+
+            }, "Id");
+            par.Add("CreatedBy", createdBy);
+            try
+            {
+                using (var _con = GetConnection())
+                {
+                    await _con.ExecuteAsync("sp_HO_SO_Them_v2", par, commandType: CommandType.StoredProcedure);
+                    var id = par.Get<int>(nameof(model.Id));
+                    return RepoResponse<int>.Create(id);
+                }
+            }
+            catch(Exception e)
+            {
+                return RepoResponse<int>.Create(0, GetException(e));
+            }
         }
     }
 }
