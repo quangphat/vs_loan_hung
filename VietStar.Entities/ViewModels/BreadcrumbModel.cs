98deb01cf0d1b5fa199d9d07d40ceddfaf76c894
quangphat <phat.dinhquang@haravan.com>
2020-08-22 02:07:36 +0700

    commandbar, Breadcrumb

---
+++ VietStar.Entities/ViewModels/BreadcrumbModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VietStar.Entities.ViewModels
+{
+    public class BreadcrumbModel
+    {
+        public string FirstPath { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+    }
+}
---
+++ VietStar.Entities/ViewModels/CommandBarButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VietStar.Entities.ViewModels
+{
+    public class CommandBarButton
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+    }
+    public class CommandBar
+    {
+        public List<CommandBarButton> Buttons { get; set; }
+    }
+}
